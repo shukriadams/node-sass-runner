daedc2f40d848902aefc7b8eeb1982179757c9f7
Shukri Adams <shukri.adams@gmail.com>
2019-04-26 16:09:02 +0200

    rewrote to be async

--- index.js
+++ index.js
@@ -1,68 +1,70 @@
-module.exports = function(options, callback) {
+module.exports = async function(options) {
+    return new Promise(function(resolve, reject){
+        let sass = require('node-sass'),
+            fs = require('fs'),
+            path = require('path'),
+            mkdirp =  require('mkdirp'),
+            glob = require('glob');
+        
+        options.globOptions = options.globOptions || {};
+        
+        if (!options.scssPath)
+            return console.error(' could not find expected value "scssPath".');
 
-    var sass = require('node-sass'),
-        fs = require('fs'),
-        path = require('path'),
-        mkdirp =  require('mkdirp'),
-        glob = require('glob');
-    
-    options.globOptions = options.globOptions || {};
+        if (!options.cssOutFolder)
+            return console.error(' could not find expected value for "cssOutFolder".');
 
-    
-    if (!options.scssPath)
-        return console.error(' could not find expected value "scssPath".');
+        if (!fs.existsSync(options.cssOutFolder))
+            mkdirp.sync(options.cssOutFolder);
 
-    if (!options.cssOutFolder)
-        return console.error(' could not find expected value for "cssOutFolder".');
+        glob(options.scssPath, options.globOptions, function (er, files) {
+            if (!files.length)
+                console.log(options.scssPath + ' dit not find any sass files.');
 
-    if (!fs.existsSync(options.cssOutFolder))
-        mkdirp.sync(options.cssOutFolder);
+            if (er)
+                return reject(er);
 
-    glob(options.scssPath, options.globOptions, function (er, files) {
-        if (!files.length)
-            console.log(options.scssPath + ' dit not find any sass files.');
-
-        if (er)
-            return callback(er);
-
-        var processedCount = 0;
-        function checkIfDone(){
-            processedCount ++;
-
-            if (processedCount === files.length){
-                callback();
-            }
-        }
-
-        files.forEach(function(file, i ){
+            let processedCount = 0;
             
-            var outfile = path.join(
-                options.cssOutFolder,
-                path.basename(file).substr(0, path.basename(file).length - 5) + '.css'); // remove .scss extension
-
-            // ignore partials
-            if (path.basename(file).substr(0, 1) === '_'){
-                checkIfDone();
-                return callback();
+            function checkIfDone(){
+                processedCount ++;
+                return processedCount >= files.length
             }
 
-            var result = sass.render({
-                file: file,
-                sourceComments: true
-            }, function(err, result){
+            files.forEach(function(file, i ){
+                
+                var outfile = path.join(
+                    options.cssOutFolder,
+                    path.basename(file).substr(0, path.basename(file).length - 5) + '.css'); // remove .scss extension
 
-                if (err){
-                    console.error('error compiling : ' + file + ' : ' + err);
-                } else {
-                    fs.writeFileSync(outfile, result.css);
-                    console.log(' compiled ' + outfile);
+                // ignore partials
+                if (path.basename(file).substr(0, 1) === '_'){
+                    if (checkIfDone())
+                        return resolve();
                 }
 
-                checkIfDone();
+                console.log('Processing ' + file) ;
+
+                sass.render({
+                    file: file,
+                    sourceComments: true
+                }, function(err, result){
+
+                    if (err){
+                        console.error(`error compiling : {file},  {err}`);
+                    } else {
+                        fs.writeFileSync(outfile, result.css);
+                        console.log(`compiled ${outfile}`);
+                    }
+                    
+                    if (checkIfDone())
+                        return resolve();
+
+                });
 
             });
 
         });
-
     });
+
 };
